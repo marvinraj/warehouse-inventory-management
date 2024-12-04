8769c3eddf6ae8ecc4f2571db36b63cdc31baaf9
Marvin <marvinraj77@gmail.com>
2024-12-04 17:48:06 +0800

    added logic for get all products and add product, testing done

--- server/controllers/inventory.controller.js
+++ server/controllers/inventory.controller.js
@@ -2,7 +2,7 @@ const db = require('../config/db');
 
 const getAllProducts = (req,res) => {
     // store sql query
-    const q = "SELECT * FROM testtable;"
+    const q = "SELECT * FROM inventory;"
     // send stored query to database
     db.query(q, (err, data) => {
         if(err) {
@@ -14,7 +14,25 @@ const getAllProducts = (req,res) => {
     });
 }
 
-const addProduct = (req,res) => {}
+const addProduct = (req,res) => {
+    const q = "INSERT INTO inventory (`name`,`description`,`quantity`,`category`,`price`) VALUES (?)"
+    const values = [
+        req.body.name,
+        req.body.description,
+        req.body.quantity,
+        req.body.category,
+        req.body.price
+    ]
+
+    db.query(q, [values], (err,data) => {
+        if(err) {
+            return res.json(err)
+        }
+        else {
+            return res.json(data)
+        }
+    });
+}
 
 const updateProduct = (req,res) => {}
 
