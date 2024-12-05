a3bf581032ed71ad11b80533021811a9541cb53a
Marvin <marvinraj77@gmail.com>
2024-12-05 09:57:11 +0800

    added outbound routes

--- server/routes/outbound.route.js
+++ server/routes/outbound.route.js
@@ -6,7 +6,7 @@ const router = express.Router();
 // list all outbounds
 router.get("/", getAllOutbounds);
 
-// add a new inbound
+// add a new outbound
 router.post("/", addOutbound)
 
 module.exports = router;
