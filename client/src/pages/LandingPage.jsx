b1ba4bf2afc2b04a0c9b9c244e7431f003c90567
Marvin <marvinraj77@gmail.com>
2024-12-09 22:31:23 +0800

    updated login and landing page

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -4,7 +4,7 @@ import BottomNav from '../components/BottomNav'
 const LandingPage = () => {
   return (
     <div className='bg-stone-900 h-screen text-gray-300'>
-        <h3 className='landing-text'>looking for a better way to manage your warehouse? <span className='text-indigo-600 block text-center text-xl'>look no further.</span></h3>
+        <h3 className='landing-text'>Looking for a better way to manage your warehouse? <span className='text-indigo-600 block text-center text-2xl'>Look no further.</span></h3>
         <BottomNav/>
     </div>
   )
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -19,49 +19,59 @@ const LoginPage = () => {
                 localStorage.setItem('username', user.username);
                 navigate("/dashboard");
             } else {
-                setErrorMessage("wrong username or password.");
+                setErrorMessage("Incorrect username or password.");
             }
         } catch (err) {
             if (err.response && err.response.data) {
                 setErrorMessage(err.response.data.message);
             } else {
-                setErrorMessage('error occurred');
+                setErrorMessage('error occurred. Please try again.');
             }
         }
     };
 
     return (
-        <div className="min-h-screen flex items-center justify-center">
-            {/* login form */}
-            <form className="max-w-sm w-full mx-auto" onSubmit={login}>
-                {/* title */}
-                <h1 className="font-bold text-2xl text-center mb-5">LOGIN FORM</h1>
-                {/* inputs & labels */}
-                <div className="mb-5">
-                    <label className="block mb-2 text-sm font-medium text-gray-900 ">Your username:</label>
-                    <input
-                        type="text"
-                        onChange={(e) => setUsername(e.target.value)}
-                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
-                        placeholder="Username"
-                        required/>
+        <div className="min-h-screen flex flex-col items-center justify-center bg-stone-900">
+            <div className="bg-white shadow-lg rounded-lg p-8 w-full max-w-sm">
+                {/* header */}
+                <div className='text-center mb-4'>
+                    <svg 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        fill="none" 
+                        viewBox="0 0 24 24" 
+                        stroke-width="1.5" 
+                        stroke="currentColor" 
+                        class="inline-block h-7 w-7 mr-2 text-indigo-600">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m21 7.5-2.25-1.313M21 7.5v2.25m0-2.25-2.25 1.313M3 7.5l2.25-1.313M3 7.5l2.25 1.313M3 7.5v2.25m9 3 2.25-1.313M12 12.75l-2.25-1.313M12 12.75V15m0 6.75 2.25-1.313M12 21.75V19.5m0 2.25-2.25-1.313m0-16.875L12 2.25l2.25 1.313M21 14.25v2.25l-2.25 1.313m-13.5 0L3 16.5v-2.25" />
+                    </svg>
+                    <a className="font-bold text-3xl text-center text-indigo-600 mb-4 font-mono">stampede</a>
                 </div>
-                <div className="mb-5">
-                    <label className="block mb-2 text-sm font-medium text-gray-900 ">Your password:</label>
-                    <input
-                        type="password"
-                        onChange={(e) => setPassword(e.target.value)}
-                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
-                        placeholder="Password"
-                        required/>
-                </div>
-                {/* login button */}
-                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">
-                    Login
-                </button>
-                {/* display error message if login fails */}
-                {errorMessage && <div className="text-red-500 text-center mt-3">{errorMessage}</div>}
-            </form>
+                <p className="text-center text-gray-600 mb-6">Login to your account</p>
+                {/* login form */}
+                <form onSubmit={login}>
+                    {/* username */}
+                    <div className="mb-4">
+                        <label className="block text-sm font-medium text-gray-700 mb-2">Username</label>
+                        <input type="text" onChange={(e) => setUsername(e.target.value)} className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500" placeholder="Enter your username" required />
+                    </div>
+                    {/* password */}
+                    <div className="mb-4">
+                        <label className="block text-sm font-medium text-gray-700 mb-2">Password</label>
+                        <input type="password" onChange={(e) => setPassword(e.target.value)} className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500" placeholder="Enter your password" required/>
+                    </div>
+                    {/* login button */}
+                    <button type="submit" className="w-full bg-indigo-600 hover:bg-indigo-700 text-white font-medium py-2 px-4 rounded-lg mt-4">
+                        Login
+                    </button>
+                    {/* display error message if login fails */}
+                    {errorMessage && (<div className="mb-4 text-red-500 text-sm text-center">{errorMessage}</div>)}
+                </form>
+            </div>
+            <button 
+                onClick={() => navigate("/")} 
+                className="mt-6 text-indigo-400 bg-gray-800 hover:bg-gray-900 font-medium py-2 px-4 rounded-lg text-sm">
+                Back to Main Page
+            </button>
         </div>
     );
 };
